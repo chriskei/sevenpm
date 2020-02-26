63936e75e12c592501d34a681c8733010f92d747
chriskei <kei.c@husky.neu.edu>
2020-02-25 21:32:35 -0500

    added category lists

--- client/src/components/RestaurantForm.js
+++ client/src/components/RestaurantForm.js
@@ -1,68 +1,71 @@
 import React from "react";
 import { Formik, Form, Field, ErrorMessage } from "formik";
-import { Slider } from "@material-ui/core";
+import { Button } from "@material-ui/core";
 
-const click = () => {
-  alert(3);
-}
+const json1 = { addedValue: "1" };
 
 const RestaurantForm = () => (
-  <Formik
-    initialValues={{
-      location: "",
+  <div>
+    <h1>add button for categories here</h1>
+    <Formik
+      initialValues={{
+        location: "",
 
-      radius: 1,
+        radius: 1,
 
-      cheap: false,
-      average: false,
-      nice: false,
-      fancy: false
-    }}
-    onSubmit={(values, { setSubmitting }) => {
-      alert(JSON.stringify(values, null, 2));
-      setSubmitting(false);
-    }}
-  >
-    {({ isSubmitting }) => (
-      <Form style={{ color: "white" }}>
-        <div>
-          <h1>Location:</h1>
-          <Field type="text" name="location"></Field>
-        </div>
+        cheap: false,
+        average: false,
+        nice: false,
+        fancy: false
+      }}
+      onSubmit={(values, { setSubmitting }) => {
+        for (var key in json1) {
+          values[key] = json1[key];
+        }
+        alert(JSON.stringify(values, null, 2));
+        setSubmitting(false);
+      }}
+    >
+      {({ isSubmitting }) => (
+        <Form style={{ color: "white" }}>
+          <div>
+            <h1>Location:</h1>
+            <Field type="text" name="location"></Field>
+          </div>
 
-        <div>
-          <h1>Radius (miles):</h1>
-          <label for="radius">1</label>
-          <Field type="range" min={1} max={25} name="radius" />
-          <label for="radius">25</label>
-        </div>
+          <div>
+            <h1>Radius (miles):</h1>
+            <label for="radius">1</label>
+            <Field type="range" min={1} max={25} name="radius" />
+            <label for="radius">25</label>
+          </div>
 
-        <div>
-          <h1>Price:</h1>
-          <Field type="checkbox" name="cheap" />
-          <label for="cheap">$ </label>
-          <Field type="checkbox" name="average" />
-          <label for="average">$$ </label>
-          <Field type="checkbox" name="nice" />
-          <label for="nice">$$$ </label>
-          <Field type="checkbox" name="fancy" />
-          <label for="fancy">$$$$</label>
-        </div>
+          <div>
+            <h1>Price:</h1>
+            <Field type="checkbox" name="cheap" />
+            <label for="cheap">$ </label>
+            <Field type="checkbox" name="average" />
+            <label for="average">$$ </label>
+            <Field type="checkbox" name="nice" />
+            <label for="nice">$$$ </label>
+            <Field type="checkbox" name="fancy" />
+            <label for="fancy">$$$$</label>
+          </div>
 
-        <div>
-          <h1>Categories:</h1>
-          <Field type="checkbox" name="african" />
-          <label for="african">$ </label>
-        </div>
+          <br />
 
-        <button id="3" onClick={click} value="3">  </button>
-
-        <button type="submit" disabled={isSubmitting}>
-          Submit
-        </button>
-      </Form>
-    )}
-  </Formik>
+          <Button
+            type="submit"
+            disabled={isSubmitting}
+            size="large"
+            variant="contained"
+          >
+            SUBMIT
+          </Button>
+        </Form>
+      )}
+    </Formik>
+  </div>
 );
 
 export { RestaurantForm };
--- index.js
+++ index.js
@@ -1,37 +1,47 @@
-const express = require('express')
-const cowsay = require('cowsay')
-const cors = require('cors')
-// Create the server
-const app = express()
-// Serve our api route /cow that returns a custom talking text cow
-app.get('/api/cow/:say', cors(), async (req, res, next) => {
-  try {
-    const text = req.params.say
-    const moo = cowsay.say({ text })
-    res.json({ moo })
-  } catch (err) {
-    next(err)
-  }
-})
-// Serve our base route that returns a Hello World cow
-app.get('/api/cow/', cors(), async (req, res, next) => {
-  try {
-    const moo = cowsay.say({ text: 'Hello World!' })
-    res.json({ moo })
-  } catch (err) {
-    next(err)
-  }
-})
-// Choose the port and start the server
-const PORT = process.env.PORT || 5000
-app.listen(PORT, () => {
-  console.log(`Mixing it up on port ${PORT}`)
-})
+import express from "express";
+import cowsay from "cowsay";
+import cors from "cors";
 
-const path = require('path')
+import displayCategories from "./categories/displayCategories";
+import searchCategories from "./categories/searchCategories";
+
+const displayCategoriesCopy = displayCategories;
+const searchCategoriesCopy = searchCategories;
+
+// Create the server
+const app = express();
+
+let chosenCategorieIndices = [];
+// Serve our api route /cow that returns a custom talking text cow
+app.get("/api/cow/:say", cors(), async (req, res, next) => {
+  try {
+    const text = req.params.say;
+    const moo = cowsay.say({ text });
+    res.json({ moo });
+  } catch (err) {
+    next(err);
+  }
+});
+// Serve our base route that returns a Hello World cow
+app.get("/api/cow/", cors(), async (req, res, next) => {
+  try {
+    const moo = cowsay.say({ text: "Hello World!" });
+    res.json({ moo });
+  } catch (err) {
+    next(err);
+  }
+});
+
+const path = require("path");
 // Serve static files from the React frontend app
-app.use(express.static(path.join(__dirname, 'client/build')))
+app.use(express.static(path.join(__dirname, "client/build")));
 // Anything that doesn't match the above, send back index.html
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname + '/client/build/index.html'))
-})
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname + "/client/build/index.html"));
+});
+
+// Choose the port and start the server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Mixing it up on port ${PORT}`);
+});
